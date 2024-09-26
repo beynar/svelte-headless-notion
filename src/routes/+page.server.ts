91370e87faf1f68d77f69f90606b3a8489f849b7
Beynar <arnaud@derbey.dev>
2024-09-26 18:06:21 +0200

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,16 @@
 import { PRIVATE_NOTION_TOKEN } from '$env/static/private';
 import { getPage, type PageWithBlocks } from '../lib/server.js';
-
+const getFromOrigin = () => {
+	return getPage({
+		auth: PRIVATE_NOTION_TOKEN,
+		id: '10b1ca42e1a8800caab2e6a73c3c11d0'
+	});
+};
 export async function load({ platform }) {
+	const page_test = await getFromOrigin();
+	return {
+		page: page_test
+	};
 	const cacheKey = new Request('https://10b1ca42e1a8800caab2e6a73c3c11d0.notion.co');
 	const cachedValue = await platform?.caches.default.match(cacheKey);
 	const isStale =
@@ -14,12 +23,6 @@ export async function load({ platform }) {
 
 	console.log({ isStale, cachedValue });
 
-	const getFromOrigin = async () => {
-		return getPage({
-			auth: PRIVATE_NOTION_TOKEN,
-			id: '10b1ca42e1a8800caab2e6a73c3c11d0'
-		});
-	};
 	const page = await getFromOrigin();
 	// const page = cachedValue ? ((await cachedValue.json()) as PageWithBlocks) : await getFromOrigin();
 
